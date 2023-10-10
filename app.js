febff7e41b1ed2956aad2590e9a7ddd2cbc3ba19
watchakorn-18k <74919942+watchakorn-18k@users.noreply.github.com>
2023-10-11 00:11:51 +0700

    Update page

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link
-      href="https://cdn.jsdelivr.net/npm/daisyui@3.9.2/dist/full.css"
-      rel="stylesheet"
-      type="text/css"
-    />
-  </head>
-  <body></body>
-  <script src="https://cdn.jsdelivr.net/gh/vanjs-org/van/public/van-1.2.1.nomodule.min.js"></script>
-  <script src="app.js"></script>
-  <script src="https://cdn.tailwindcss.com"></script>
-</html>
+
+const { a, div, li, p, ul, h1, button, title, span } = van.tags;
+
+const DateTime = () => {
+  const [hours, minute, second] = [van.state(0), van.state(0), van.state(0)];
+  let id;
+  const start = () =>
+    (id =
+      id ||
+      setInterval(() => {
+        const currentDate = new Date();
+        hours.val = currentDate.getHours();
+        minute.val = currentDate.getMinutes();
+        second.val = currentDate.getSeconds();
+      }, 1000));
+  start();
+  return div({ class: "m-8" }, () =>
+    span(
+      { class: "countdown font-mono text-3xl" },
+      span({ style: () => `--value:${hours.val}` }),
+      ":",
+      span({ style: () => `--value:${minute.val}` }),
+      ":",
+      span({ style: () => `--value:${second.val}` })
+    )
+  );
+};
+const Hello = () =>
+  div(
+    { class: "max-w-md" },
+    DateTime({}),
+    h1({ class: "text-3xl font-bold" }, "👋Hello, I'm wk18k"),
+    p(
+      { class: "py-6" },
+      "We're committed to providing you with the best possible experience."
+    ),
+    button({ class: "btn btn-primary" }, "Go to my github")
+  );
+
+const HeroPage = () =>
+  div(
+    { class: "hero min-h-screen bg-base-200 font-mono" },
+
+    div({ class: "hero-content text-center" }, Hello(), title("WK-18K"))
+  );
+
+const Main = () => div(HeroPage());
+van.add(document.body, Main());
