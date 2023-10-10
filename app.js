cd3acfd2deb14601a119621a0bf682821c2faab1
watchakorn-18k <74919942+watchakorn-18k@users.noreply.github.com>
2023-10-11 01:08:27 +0700

    Update page

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-
-const { a, div, li, p, ul, h1, button, title, span } = van.tags;
+const { a, div, li, p, ul, h1, button, title, span, img } = van.tags;
 
 const DateTime = () => {
   const [hours, minute, second] = [van.state(0), van.state(0), van.state(0)];
@@ -16,7 +15,7 @@ const DateTime = () => {
   start();
   return div({ class: "m-8" }, () =>
     span(
-      { class: "countdown font-mono text-3xl" },
+      { class: "countdown font-mono text-4xl" },
       span({ style: () => `--value:${hours.val}` }),
       ":",
       span({ style: () => `--value:${minute.val}` }),
@@ -25,24 +24,36 @@ const DateTime = () => {
     )
   );
 };
+
 const Hello = () =>
   div(
     { class: "max-w-md" },
-    DateTime({}),
-    h1({ class: "text-3xl font-bold" }, "👋Hello, I'm wk18k"),
+    DateTime(),
+    h1({ class: "text-2xl font-bold" }, "👋Hello, I'm wk18k"),
     p(
       { class: "py-6" },
       "We're committed to providing you with the best possible experience."
     ),
-    button({ class: "btn btn-primary" }, "Go to my github")
+    a(
+      { href: "https://github.com/watchakorn-18k" },
+      button({ class: "btn btn-primary" }, "Go to my github")
+    )
   );
 
-const HeroPage = () =>
-  div(
+const getIP = (json) => {
+  document.title += ` | ${json.ip}`;
+};
+
+const HeroPage = () => {
+  fetch("https://checkip.amazonaws.com/")
+    .then((res) => console.log(res.text()))
+    .then((data) => console.log(data));
+  return div(
     { class: "hero min-h-screen bg-base-200 font-mono" },
 
-    div({ class: "hero-content text-center" }, Hello(), title("WK-18K"))
+    div({ class: "hero-content text-center" }, Hello(), title("watchakorn-18k"))
   );
+};
 
 const Main = () => div(HeroPage());
 van.add(document.body, Main());
